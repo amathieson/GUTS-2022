8fdc7520d8d5a0af12eebbb82fb7425407c69cbe
Edoardo Luciani <edoardo.luciani@gmail.com>
2022-02-11 22:02:05 +0000

    Completed sendAudio

--- src/main/java/tech/spatialcomm/client/Client.java
+++ src/main/java/tech/spatialcomm/client/Client.java
@@ -4,19 +4,25 @@ import tech.spatialcomm.commands.CmdPong;
 import tech.spatialcomm.commands.Commands;
 import tech.spatialcomm.io.IOHelpers;
 
-import java.net.DatagramSocket;
-import java.net.Socket;
+import java.net.*;
+import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
+import java.util.Random;
 
 public class Client {
     String ip;
-    int port;
+    int tcp_port;
+    final int udp_port = 25567;
+
     private Socket clientSocket;
+    private DatagramSocket udpClientSocket;
+
     private int clientId;
+    long counter = 0;
 
     public Client(String ip, int port, String name) {
         this.ip = ip;
-        this.port = port;
+        this.tcp_port = port;
         Socket clientSocket = null;
         try {
             // Send a connection request
@@ -30,6 +36,8 @@ public class Client {
             assert IOHelpers.readInt16(clientSocket.getInputStream()) == Commands.CONNECT_OK.id;
             IOHelpers.readInt32(clientSocket.getInputStream());
             clientId = IOHelpers.readInt32(clientSocket.getInputStream());
+
+            udpClientSocket = new DatagramSocket(udp_port, InetAddress.getByName(ip));
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -46,6 +54,19 @@ public class Client {
                 Thread.sleep(100);
             }
             catch (Exception ignored) {}
+        }
+    }
+
+    public void sendAudio() {
+        try {
+            byte[] audio = new byte[400];
+            Random rand = new Random();
+            rand.nextBytes(audio);
+            byte[] message = ByteBuffer.allocate(800).putInt(clientId).putLong(counter).put(audio).array();
+            DatagramPacket dp = new DatagramPacket(message, message.length);
+            udpClientSocket.send(dp);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
