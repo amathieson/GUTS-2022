566ef46b79b03a33c0410ea4df40d39cfb794b8e
octeep <octeep@pm.me>
2022-02-11 19:47:35 +0000

    add socket stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,31 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Main {
-    public static void main(String[] main) {
-        System.out.println("Hello world");
+
+    private static final ExecutorService SERVICE = Executors.newCachedThreadPool();
+
+    public static void main(String[] main) throws Exception {
+        System.out.println("Among us");
+        try (ServerSocket socket = new ServerSocket(25567)) {
+            var client = socket.accept();
+            System.out.println("Connection received from " + client.getRemoteSocketAddress());
+            SERVICE.submit(() -> handleClient(client));
+        }
     }
+
+    public static void handleClient(Socket client) {
+        try {
+            System.out.println("handling client");
+            client.getOutputStream().write("bye".getBytes(StandardCharsets.UTF_8));
+            Thread.sleep(5000L);
+            client.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
