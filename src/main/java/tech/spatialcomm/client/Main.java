362ae51a09f9bd0af82b218b60c2851221cd1099
Edoardo Luciani <edoardo.luciani@gmail.com>
2022-02-11 22:00:42 +0000

    Completed sendAudio

--- src/main/java/tech/spatialcomm/client/Main.java
+++ src/main/java/tech/spatialcomm/client/Main.java
@@ -2,10 +2,9 @@ package tech.spatialcomm.client;
 
 public class Main {
     public static void main(String args[]) {
-        Client client = new Client("127.0.0.1", 6000, "boi");
+        Client client = new Client("127.0.0.1", 25567, "boi");
         Thread t = new Thread(client::listenToPing);
         t.start();
-
-
+        client.sendAudio();
     }
 }
